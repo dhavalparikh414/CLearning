4ea7a67d1155c9b437de03483a079c5b6077e141
Dhaval Parikh <dhavalparikh414@gmail.com>
2025-12-15 16:35:01 +1100

    Initial C learning document

---
+++ CLearningPart1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello World!!");
+	
+	// This is a single line comment in C
+	
+	/* If you want to write multi line
+	comment then use this style
+	this is part of multiline comment*/
+}
