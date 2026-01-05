2ff9560c6c6206e3c312605e6cc031d885b29132
Dhaval Parikh <dhavalparikh414@gmail.com>
2026-01-05 17:14:29 +1100

    updated preprocessor and compiler differences

--- CLearningPart1.c
+++ CLearningPart1.c
@@ -9,4 +9,41 @@ int main()
 	/* If you want to write multi line
 	comment then use this style
 	this is part of multiline comment*/
+	
+	/* Features of C  are as follows
+	1) Portability
+	2) Modularity
+	3) Middle level
+	4) Efficient
+	5) Fixed keywords (32)
+	6) Structured programming language
+	7) Powerful data structures
+	*/
+	
+/* ------------- Compilation Process ------------
+ Editing source code -- ex : a.c
+ Pre processor -- ex : a.i
+ Compiler -- ex: a.s
+ Assembler -- ex: a.o
+ Linker
+ Executable file -- ex: a.out is the default executable file name of gcc compiler.
+ 
+ ------------------------------------------------*/
+ 
+ /* -------- Difference between pre processor and compiler --------------
+ 
+ Pre processor :
+ 1) creates intermediate code
+ 2) includes header files
+ 3) removes comments
+ 4) defines macros
+ 5) conditional compilation 
+ 
+ Compiler :
+ 1) checks syntax errors
+ 2) do some optimisation
+ 3) creates assembly code
+ */
+ 
+	
 }
