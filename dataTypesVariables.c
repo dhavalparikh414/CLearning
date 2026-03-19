1d8b3c90980763f8608a378d3fbb73685ad01ddf
dhavalparikh414 <dhavalparikh414@gmail.com>
2026-03-19 18:16:30 +1100

    learning data types and variables in C

---
+++ dataTypesVariables.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int g1; // C allows a global variable to be declared again when first declaration does not intialise the variable
+int g1=10;
+int main()
+{
+    printf("the value of global variable is %d\n",g1);
+    /*The range of variables of different data types depends on its size
+
+    Ex : signed char is 1 byte in size so it can hold values from -128 to +127
+    unsigned char can hold from 0 to 255.
+
+    Any unsigned data type can hold values from 0 to 2^bits - 1
+    Any signed data type can hold values from -2^(bits-1) to  2^(bits-1) -1
+    */
+
+    /*Every char has an integer value (ascii value) */
+
+    char ch1;
+    printf("Enter a character\n");
+    scanf("%c", &ch1);
+    printf("The entered character is %c and its ascii value is %d\n", ch1, ch1);
+
+    return 0;
+}
